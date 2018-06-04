f3fa6626c7c38f360c6a2b7f20a4051f99937cd7
LennyDuan <hod8@aber.ac.uk>
2018-06-04 20:21:08 +0100

    init output

---
+++ init/src/api/output.js
@@ -0,0 +1,9 @@
+import { log, debug } from '../util/logger';
+
+export
+const logOutput = async (payload, context, callback, {} = {}) => {
+  debug(payload);
+  debug(context);
+  debug(callback);
+  callback(null, '200');
+};
