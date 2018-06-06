77dfcb748f0db416f9c68675f378a815fd4111b8
LennyDuan <hod8@aber.ac.uk>
2018-06-06 22:31:45 +0100

    aws skd

--- init/src/api/output.js
+++ init/src/api/output.js
@@ -1,9 +1,14 @@
 const debug = require('../util/logger').debug
+const AWS = require('aws-sdk');
+const docClient = new AWS.DynamoDB.DocumentClient({region: 'eu-west-1'});
 
 export
 const logOutput = async (payload, context, callback) => {
-  debug(payload);
-  debug(context);
-  debug(callback);
-  callback(null, '200');
+  const scanningParameters = {
+       TableName: 'universities.university',
+   }
+   console.log('Read DDB');
+   const data = await docClient.scan(scanningParameters).promise();
+   
+   callback(null,data)
 };
