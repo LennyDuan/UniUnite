aec4a55e71cbd3c995b93aacf73fc65f4167b8d6
LennyDuan <hod8@aber.ac.uk>
2018-07-13 14:11:38 +0100

    fix lint

--- UniPlatform/projects/user/ddb/scan.js
+++ UniPlatform/projects/user/ddb/scan.js
@@ -14,19 +14,19 @@ exports.scanAll = async (tableName) => {
 
 exports.scanOne = async (tableName, name, value) => {
   const scanningParameters = {
-   TableName: tableName,
-   ExpressionAttributeNames: {
-    "#sn": name,
-   },
-   ExpressionAttributeValues: {
-    ":sv":value,
-   },
-   FilterExpression: "#sn = :sv",
- };
- console.log(`Scan DDB table: ${tableName} for ${name} - ${value}`);
- console.log('#############');
- const data = await docClient.scan(scanningParameters).promise();
- console.log(JSON.stringify(data));
- console.log('#############');
- return data.Items;
+    TableName: tableName,
+    ExpressionAttributeNames: {
+      '#sn': name,
+    },
+    ExpressionAttributeValues: {
+      ':sv': value,
+    },
+    FilterExpression: '#sn = :sv',
+  };
+  console.log(`Scan DDB table: ${tableName} for ${name} - ${value}`);
+  console.log('#############');
+  const data = await docClient.scan(scanningParameters).promise();
+  console.log(JSON.stringify(data));
+  console.log('#############');
+  return data.Items;
 };
