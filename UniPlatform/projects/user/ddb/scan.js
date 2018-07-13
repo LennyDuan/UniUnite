64943cf4b4e78860e6e4906026a67a67a2065cf5
LennyDuan <hod8@aber.ac.uk>
2018-07-13 13:56:23 +0100

    init ddb util

--- UniPlatform/projects/user/ddb/scan.js
+++ UniPlatform/projects/user/ddb/scan.js
@@ -14,12 +14,19 @@ exports.scanAll = async (tableName) => {
 
 exports.scanOne = async (tableName, name, value) => {
   const scanningParameters = {
-    TableName: tableName,
-  };
-  console.log(`Read DDB table: ${tableName}`);
-  console.log('#############');
-  const data = await docClient.scan(scanningParameters).promise();
-  console.log(JSON.stringify(data));
-  console.log('#############');
-  return data.Items;
+   TableName: tableName,
+   ExpressionAttributeNames: {
+    "#sn": name,
+   },
+   ExpressionAttributeValues: {
+    ":sv":value,
+   },
+   FilterExpression: "#sn = :sv",
+ };
+ console.log(`Scan DDB table: ${tableName} for ${name} - ${value}`);
+ console.log('#############');
+ const data = await docClient.scan(scanningParameters).promise();
+ console.log(JSON.stringify(data));
+ console.log('#############');
+ return data.Items;
 };
