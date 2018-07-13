4e955c5ac9652a7ac5a36210d73353001577e40f
LennyDuan <hod8@aber.ac.uk>
2018-07-13 13:26:36 +0100

    finish put

--- UniPlatform/projects/user/ddb/put.js
+++ UniPlatform/projects/user/ddb/put.js
@@ -1,12 +1,12 @@
 const docClient = require('./config').docClient;
 
 exports.put = async (tableName, item) => {
-   const putParameters = {
-      Item: item,
-      TableName: tableName,
-    }
-    console.log(`Put item ${item}  to DDB table: ${tableName}`);
-    console.log('#############');
-    const data = await docClient.put(putParameters).promise()
-    console.log('#############');
-}
+  const putParameters = {
+    Item: item,
+    TableName: tableName,
+  };
+  console.log(`Put item ${item}  to DDB table: ${tableName}`);
+  console.log('#############');
+  const data = await docClient.put(putParameters).promise();
+  console.log('#############');
+};
--- UniPlatform/projects/user/src/put.js
+++ UniPlatform/projects/user/src/put.js
@@ -6,8 +6,8 @@ const putItem = require('../ddb/scan').put;
 exports.put = async (event, context, callback) => {
   try {
     const item = event.body;
-    const data = await putItem(tableName, item);
-    callback(null, "Create/Update successful");
+    await putItem(tableName, item);
+    callback(null, 'Create/Update successful');
   } catch (error) {
     console.log(error);
     callback(error);
