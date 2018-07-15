42b35ef36246b4d5b4520809b45f342519328424
LennyDuan <hod8@aber.ac.uk>
2018-07-15 16:03:20 +0100

    init verify

---
+++ UniPlatform/projects/user_credential/src/verify.js
@@ -0,0 +1,39 @@
+const md5 = require('md5');
+const moment = require('moment');
+const config = require('./config').config;
+
+const userTableName = config.userTableName;
+const credentialTableName = config.credentialTableName;
+const dateFormat = config.dateFormat;
+const expiredTime = config.expiredTime;
+const getUser = require('../ddb/get').get;
+const createCredential = require('../ddb/put').put;
+
+exports.login = async (event, context, callback) => {
+  try {
+    const account = event.queryStringParameters.account;
+    const credential = event.queryStringParameters.credential;
+    const user = await getUser(userTableName, account);
+    console.log(`Get user: ${JSON.stringify(user)}`);
+    const password = user.password;
+    const correctCredential = md5(password);
+    console.log(`Correct credential for this user: ${correctCredential}`);
+    if (correctCredential !== credential) {
+      throw new Error(`Invalid account or password: ${account} - ${credential}`);
+    } else {
+      console.log(`Verified user: ${account} - ${correctCredential}`);
+      const item = {
+        account,
+        credential,
+        date_started: moment().format(dateFormat),
+        date_expired: moment().add('days', expiredTime).format(dateFormat),
+      };
+      console.log(`New credential: ${JSON.stringify(item)}`);
+      await createCredential(credentialTableName, item);
+      callback(null, item);
+    }
+  } catch (error) {
+    console.log(error);
+    callback(error);
+  }
+};
