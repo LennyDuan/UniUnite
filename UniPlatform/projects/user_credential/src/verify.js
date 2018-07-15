f8041503a65cc2b400e8c329157ae562bb934bf5
LennyDuan <hod8@aber.ac.uk>
2018-07-15 16:14:59 +0100

    init verify

--- UniPlatform/projects/user_credential/src/verify.js
+++ UniPlatform/projects/user_credential/src/verify.js
@@ -1,37 +1,32 @@
-const md5 = require('md5');
 const moment = require('moment');
 const config = require('./config').config;
+const getCredential = require('../ddb/get').get;
 
-const userTableName = config.userTableName;
 const credentialTableName = config.credentialTableName;
 const dateFormat = config.dateFormat;
-const expiredTime = config.expiredTime;
-const getUser = require('../ddb/get').get;
-const createCredential = require('../ddb/put').put;
 
-exports.login = async (event, context, callback) => {
+exports.verify = async (event, context, callback) => {
   try {
     const account = event.queryStringParameters.account;
     const credential = event.queryStringParameters.credential;
-    const user = await getUser(userTableName, account);
-    console.log(`Get user: ${JSON.stringify(user)}`);
-    const password = user.password;
-    const correctCredential = md5(password);
+    const userCredential = await getCredential(userTableName, account);
+    if(!userCredential) {
+      throw new Error(`Invalid account: ${account}`);
+    }
+    console.log(`Get user credential: ${JSON.stringify(userCredential)}`);
+    const correctCredential = userCredential.credential;
     console.log(`Correct credential for this user: ${correctCredential}`);
     if (correctCredential !== credential) {
       throw new Error(`Invalid account or password: ${account} - ${credential}`);
-    } else {
-      console.log(`Verified user: ${account} - ${correctCredential}`);
-      const item = {
-        account,
-        credential,
-        date_started: moment().format(dateFormat),
-        date_expired: moment().add('days', expiredTime).format(dateFormat),
-      };
-      console.log(`New credential: ${JSON.stringify(item)}`);
-      await createCredential(credentialTableName, item);
-      callback(null, item);
     }
+
+    const currentDate = moment().format(dateFormat);
+    const expiredDate = moment(userCredential.date_expired, dateFormat);
+    console.log(`Current date [${currentDate}] - Expired date [${expiredDate}]`);
+    if (currentDate.isAfter(expiredDate)) {
+      throw new Error(`Credential Expired: ${expiredDate}`);
+    }
+    callback(null, true);
   } catch (error) {
     console.log(error);
     callback(error);
