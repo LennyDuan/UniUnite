3323e095feafed4b5fc8dd4f93cdb83694767c7f
LennyDuan <hod8@aber.ac.uk>
2018-07-15 15:37:38 +0100

    init md5 & moment

--- UniPlatform/projects/user_credential/src/login.js
+++ UniPlatform/projects/user_credential/src/login.js
@@ -1,3 +1,5 @@
+const md5 = require('md5');
+const moment = require('moment');
 const config = require('./config').config;
 const userTableName = config.userTableName;
 const credentialTableName = config.credentialTableName;
@@ -11,8 +13,19 @@ exports.login = async (event, context, callback) => {
     const user = await getUser(tableName, account);
     console.log(`Get user: ${JSON.stringify(user)}`);
     const password = user.password
-    
-    callback(null, data);
+    const correctCredential = md5(password);
+    console.log(`Correct credential for this user: ${correctCredential}`);
+    if (correctCredential !== credential) {
+        throw error(`Invalid account or password: ${account} - ${credential}`)
+    } else {
+      console.log(`Verified user: ${account} - ${correctCredential}`);
+      const item = {
+        account,
+        credential,
+        
+      }
+    }
+    throw error(`Unexpected Error`)
   } catch (error) {
     console.log(error);
     callback(error);
